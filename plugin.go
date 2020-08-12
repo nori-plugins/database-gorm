b7c1c95f5f3d3aa8bdeb83a6b8eabcb2475c4fb4
bruteforce1414 <bruteforce1414@gmail.com>
2020-08-12 10:22:43 +0500

    refactor: meta

--- plugin.go
+++ plugin.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+
 	"github.com/nori-io/sql-gorm/internal/hook"
 
 	"github.com/jinzhu/gorm"
@@ -61,7 +62,7 @@ func (p *service) Instance() interface{} {
 }
 
 func (p *service) Meta() meta.Meta {
-	return &meta.Data{
+	return meta.Data{
 		ID: meta.ID{
 			ID:      "sql/gorm",
 			Version: "1.9.15",
@@ -85,12 +86,12 @@ func (p *service) Meta() meta.Meta {
 				Type:  "GPLv3",
 				URI:   "https://www.gnu.org/licenses/"},
 		},
-		Links:      nil,
+		Links: []meta.Link{},
 		Repository: meta.Repository{
 			Type: "git",
 			URI:  "https://github.com/nori-io/sql-gorm",
 		},
-		Tags:       []string{"orm", "gorm", "sql", "database", "db"},
+		Tags: []string{"orm", "gorm", "sql", "database", "db"},
 	}
 }
 
